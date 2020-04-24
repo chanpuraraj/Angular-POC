864b2484ae354095c9b3ed059f59e246d82a1fa0
Raj Chanpura <chanpur@uwindsor.ca>
2020-04-25 02:30:49 +0530

    First commit in angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path : '', component: ReactiveFormsComponent},
+      {path : 'reactive-form', component:ReactiveFormsComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
